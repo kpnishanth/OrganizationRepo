1b66c210c9e4771b2b2c6a5d7de599580f47cf84
kpnishanth <nishanth.prabhakaran@gmail.com>
2024-03-06 22:30:35 -0500

    intial commit

--- src/main/java/org/example/model/Organization.java
+++ src/main/java/org/example/model/Organization.java
@@ -1,16 +1,19 @@
 package org.example.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.springframework.hateoas.*;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
 @Entity
 @Data
 
 public class Organization extends RepresentationModel<Organization> {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String organizationId;
     private String organizationName;
     private String licenseId;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO ORGANIZATION (id, organization_Name, license_Id)
+VALUES ('122','ICE','89');
+INSERT INTO ORGANIZATION (id, organization_Name, license_Id)
+VALUES ('65','INFOSYS','45');
+INSERT INTO ORGANIZATION (id, organization_Name, license_Id)
+VALUES ('6545','WIPRO','66');
+INSERT INTO ORGANIZATION (id, organization_Name, license_Id)
+VALUES ('87','Pixel','458');
